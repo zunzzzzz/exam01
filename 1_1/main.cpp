80593836847623a85a6fd5d5d7c4dcdd2d6dd056
zunzzz <a0983208130@gmail.com>
2020-04-01 17:25:50 +0800

    new ver

--- 1_1/main.cpp
+++ 1_1/main.cpp
@@ -1,21 +1,44 @@
-// uLCD-144-G2 basic text demo program for uLCD-4GL LCD driver library
-//
+
 #include "mbed.h"
 #include "uLCD_4DGL.h"
 
-uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
+// uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
+PwmOut PWM1(D0);
+Serial pc( USBTX, USBRX );
 
 int main()
 {
-    // basic printf demo = 16 by 18 characters on screen
-    uLCD.printf("106060024\n"); //Default Green on black text
+    
+    // uLCD.printf("106060024\n"); //Default Green on black text
 
   
-    uLCD.locate(1,2);
-    uLCD.line(12, 12 , 52, 12, GREEN);
-    uLCD.line(12, 12 , 12, 52, GREEN);
-    uLCD.line(52, 52, 52, 12, GREEN);
-    uLCD.line(52, 52, 12, 52, GREEN);
-    // uLCD.triangle(1+10, 2+10, 1+10, 4+10, 3+10, 4+10, GREEN);
-    // uLCD.triangle(3+10, 2+10, 1+10, 2+10, 3+10, 4+10, GREEN);
+    // uLCD.locate(1,2);
+    // uLCD.line(12, 12 , 52, 12, GREEN);
+    // uLCD.line(12, 12 , 12, 52, GREEN);
+    // uLCD.line(52, 52, 52, 12, GREEN);
+    // uLCD.line(52, 52, 12, 52, GREEN);
+
+    float value = 0;
+    bool flag = true;
+    while(1) {
+        PWM1.period(0.001);
+        PWM1 = value;
+        // if(flag) {
+        //     value += 0.1;
+        //     if(value > 1) {
+        //         value = 0.9;
+        //         flag = false;
+        //     }
+        // }
+        // else {
+        //     value -= 0.1;
+        //     if(value < 0) {
+        //         value = 0.1;
+        //         flag = true;
+        //     }
+        // }
+        pc.printf("%1.3f\r\n", PWM1);
+        wait(0.1);
+    }
+    
 }
---
+++ code/main.cpp
@@ -0,0 +1,46 @@
+
+#include "mbed.h"
+#include "uLCD_4DGL.h"
+
+// uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
+PwmOut PWM1(D0);
+Serial pc( USBTX, USBRX );
+
+int main()
+{
+    
+    // uLCD.printf("106060024\n"); //Default Green on black text
+
+  
+    // uLCD.locate(1,2);
+    // uLCD.line(12, 12 , 52, 12, GREEN);
+    // uLCD.line(12, 12 , 12, 52, GREEN);
+    // uLCD.line(52, 52, 52, 12, GREEN);
+    // uLCD.line(52, 52, 12, 52, GREEN);
+
+    float value = 0;
+    int count = 0;
+    bool flag = true;
+    while(1) {
+        PWM1.period(0.001);
+        PWM1 = value;
+        if(flag) {
+            value += 0.1;
+            if(value > 1.1) {
+                value = 0.9;
+                flag = false;
+            }
+        }
+        else {
+            value -= 0.1;
+            if(value < 0.1) {
+                value = 0;
+                flag = true;
+            }
+        }
+        if(count < 128) pc.printf("%1.3f\r\n", value);
+        count++;
+        wait(0.1);
+    }
+    
+}
